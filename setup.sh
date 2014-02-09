7e038d57038d5411a7c4126a2515484122e5f672
ssl-umd <ssl.mxsuit@gmail.com>
2014-02-08 21:09:50 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,12 +8,13 @@ cd orocos
 mkdir orocos-toolchain
 cd orocos-toolchain
 
-gem install hoe
-
 echo "Pulling bootstrap"
 wget -O bootstrap-2.6.sh http://gitorious.org/orocos-toolchain/build/raw/toolchain-2.6:bootstrap.sh
 echo "Running bootstrap script"
 bash bootstrap-2.6.sh
+
+echo "installing hoe gem"
+gem install hoe
 
 echo "editing Rakefile"
 sed '27s/Utilrb.doc/#Utilrb.doc/' utilrb/Rakefile
